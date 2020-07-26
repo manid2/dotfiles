e624cf1a64ad8fc56d67245a78c36f729964c225
Mani Kumar <maniyoung.d2@gmail.com>
2020-07-27 00:42:48 +0530

    fix: Use full paths to source file
    Verified by sourcing system config sym link file.
    It loads the system specifc configs in system independent way.

--- dot-config/dot-bash-m/bash_system_config_wrapper.sh
+++ dot-config/dot-bash-m/bash_system_config_wrapper.sh
@@ -1,4 +1,4 @@
 # sym link to system specific bashrc here
-if [ -f bash_system_config_sym_link.sh ]; then
-    source bash_system_config_sym_link.sh # sym link to system bashrc
+if [ -f ~/.config/.bash_system_config_sym_link.sh ]; then
+    source ~/.config/.bash_system_config_sym_link.sh
 fi
