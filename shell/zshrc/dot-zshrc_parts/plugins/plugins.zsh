7b7002ebb05a69badbe8a20989538330f0384355
Mani Kumar <maniyoung.d2@gmail.com>
2021-05-06 21:14:15 +0530

    rc: disable vimrc, zshrc plugins
    * zsh auto-suggestions plugin
    * vim-syntastic plugin

--- shell/zshrc/dot-zshrc_parts/plugins/plugins.zsh
+++ shell/zshrc/dot-zshrc_parts/plugins/plugins.zsh
@@ -84,11 +84,11 @@ done
 
 # Source zsh-autosuggestions plugin if available
 # NOTE: this should be sourced at the end as suggested in official docs
-if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-    . /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-    # change suggestion color
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
-fi
+#if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+#    . /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+#    # change suggestion color
+#    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
+#fi
 
 # Source command-not-found plugin if available
 if [ -f /etc/zsh_command_not_found ]; then
