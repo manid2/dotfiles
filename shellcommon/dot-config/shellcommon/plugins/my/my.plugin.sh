0ec873e17883b223f6bd77b38d71d1eb54825069
Mani Kumar <manid2.work@gmail.com>
2022-06-19 15:41:48 +0530

    feat: [shellcommon] Add func 'lnr' to create links
    Use this function to create links using relative paths.  Provide aliases
    for creating soft link and forced soft links.

--- shellcommon/dot-config/shellcommon/plugins/my/my.plugin.sh
+++ shellcommon/dot-config/shellcommon/plugins/my/my.plugin.sh
@@ -13,6 +13,13 @@ alias mydp="cd \"$mydp\""
 alias myrp="cd \"$myrp\""
 alias mybp="cd \"$mybp\""
 
+lnr () {
+	ln $1 `realpath --relative-to=$2 $3` $4
+}
+
+alias lnrs='lnr -s '
+alias lnrsf='lnr -sf '
+
 slugify () {
 	echo "$1" | sed -E 's/[~\^]+//g' | sed -E 's/[^a-zA-Z0-9]+/-/g'
 }
