7611c081f6a1394c6214239dc0ecdc14e04efc63
Mani Kumar <manid2.work@gmail.com>
2022-06-21 10:50:35 +0530

    feat: [shellcommon] Add local script env vars

--- shellcommon/dot-config/shellcommon/local/local.sh
+++ shellcommon/dot-config/shellcommon/local/local.sh
@@ -1,3 +1,7 @@
+# add env variables needed by some useful tools
+export FZF_DEFAULT_COMMAND='fd'
+export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
+
 # put all user local apps in this script.
 if [ -f ~/.local/bin/user-local-apps ]; then
 	source ~/.local/bin/user-local-apps
