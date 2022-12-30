fd5918df64f46afab2430f3e296ca8b9f0840744
Mani Kumar <manid2.work@gmail.com>
2022-12-30 23:51:04 +0530

    refactor: [bashrc] Bash prompt is improved
    List of changes made for this improvement:
    
    * Remove redundant functions and use single function to create the prompt.
    * Add support for process exit code in prompt.
    * Change PS2 and PS4 prompts.

--- bashrc/dot-config/bashrcparts/prompt/prompt.bash
+++ bashrc/dot-config/bashrcparts/prompt/prompt.bash
@@ -1,100 +1,92 @@
-# Prompt customization wrapper
+# bash prompt file, to be sourced into ~/.bashrc
 
 # Shell common colors
 if [ -f ~/.config/shellcommon/common/colors.sh ]; then
 	source ~/.config/shellcommon/common/colors.sh
 fi
 
-# __head_ps1 = debian_chroot + user + host + cwd
-make_color_head_ps1 () {
-	local __user_and_host="$GREEN\u@\h"
-	local __cur_location="$BLUE\W" # W: cwd, w: full path to cwd
-
-	local __head_ps1="${debian_chroot:+($debian_chroot)}"
-	__head_ps1+="$__user_and_host:$__cur_location"
-	echo "$__head_ps1"
-}
-
-# __tail_ps1 = __prompt_tail
-make_color_tail_ps1 () {
-	local __prompt_tail="$VIOLET$"
-	local __ps1_color_reset="$RESET"
-
-	local __tail_ps1="$__prompt_tail"
-	__tail_ps1+="$__ps1_color_reset"
-	__tail_ps1+=" "
-	echo "$__tail_ps1"
-}
-
-# __ps1_line = __head_ps1 + __body_ps1_git + __tail_ps1
-make_color_prompt_line () {
-	local __ps1_line="$(make_color_head_ps1)"
-
-	__ps1_line+="$RESET$(__git_ps1 ' (%s)')"
-
-	__ps1_line+="$(make_color_tail_ps1)"
-	PS1="$__ps1_line"
-}
-
-# make colorless prompt line
-make_colorless_prompt_line () {
-	local __head_ps1="${debian_chroot:+($debian_chroot)}[\u@\h "
-	local __body_ps1='\W]'
-	local __tail_ps1='$ '
-	local __ps1_line="$__head_ps1$__body_ps1"
-
-	__ps1_line+="$(__git_ps1 ' (%s)')"
-
-	__ps1_line+="$__tail_ps1"
-	PS1=$__ps1_line
-}
-
 # Variable to identify the chroot for display in prompt
 if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
 	debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# Set the terminal title
-set_terminal_title () {
-	local __xterm_title=''
-	# Check if this is an xterm
-	case "$TERM" in
-		xterm* | rxvt*)
-			__xterm_title='\[\e]0;\u@\h:\w\a\]'
-			;;
-		*)
-			__xterm_title=''
-			;;
-	esac
-	PS1=${__xterm_title}${PS1}
-}
-
 # Check if terminal supports color prompt
 case "$TERM" in
 	xterm-color | *-256color)
-		color_prompt=yes
+		color_prompt='yes'
 		;;
 esac
 
-# make prompt wrapper
-make_prompt_line () {
-	if [ "$color_prompt" = yes ]; then
-		make_color_prompt_line
-	else
-		make_colorless_prompt_line
-	fi
-}
+# Reset the color variables when terminal has no support.
+if [ "$color_prompt" != 'yes' ]; then
+	MAGENTA=''
+	GREEN=''
+	BLUE=''
+	RESET=''
+fi
 
-# wrapper function
+# This function executes before displaying the prompt.
 prompt_command () {
-	make_prompt_line
-	set_terminal_title
+	# this must be the first line to capture exit code of last executed
+	# command.
+	local _proc_exit=$?
+
+	# initliaze local ps1
+	local _ps1=''
+
+	# add chroot if available
+	if  [ -n "$debian_chroot" ]; then
+		_ps1+="$BLUE(${debian_chroot:+($debian_chroot)})$RESET"
+		_ps1+="$BLUE-$RESET"
+	fi
+
+	# add user, host and current working directory
+	_ps1+="$GREEN[\u@\h:\W]$RESET"
+	_ps1+="$BLUE-$RESET"
+
+	# add process exit code
+	if [ "$_proc_exit" -eq 0 ]; then
+		_ps1+="$GREEN[$_proc_exit]$RESET"
+		_ps1+="$BLUE-$RESET"
+	else
+		_ps1+="$MAGENTA[$_proc_exit]$RESET"
+		_ps1+="$BLUE-$RESET"
+	fi
+
+	# add git status prompt
+	if [ "$(command -v __git_ps1)" ]; then
+		local _gps1=$(__git_ps1 '(%s)')
+		if  [ -n "$_gps1" ]; then
+			_ps1+="$BLUE$_gps1$RESET"
+			_ps1+="$BLUE-$RESET"
+		fi
+	fi
+
+	# add user prompt character '$'
+	_ps1+="$VIOLET$ $RESET"
+
+	# set terminal title
+	local _xterm_title=''
+	case "$TERM" in
+		xterm* | rxvt*)
+			_xterm_title='\[\e]0;\u@\h:\W\a\]'
+			;;
+		*)
+			_xterm_title=''
+			;;
+	esac
+
+	# set the prompt environment variables
+	PS1="${_xterm_title}${_ps1}"
+	PS2='> '
+	PS4='+ '
 }
 
 # `trap` with DEBUG manipulates prompt attributes on output from bash commands
 #trap 'tput sgr0' DEBUG
 
-# PROMPT_COMMAND colors the prompt text and bash commands
+# Bash always executes the contents of this environment variable here it is
+# 'prompt_command ()' just before displaying the prompt.
 export PROMPT_COMMAND=prompt_command
 
 # Shell common prompt configs
