89a4b896f24fdcb48b317b9b73eaeaa70839a817
Mani Kumar <manid2.work@gmail.com>
2023-01-28 22:07:13 +0530

    refactor: [bashrc] Bind CTRL-L to 'clear-display'
    Use bash readline command 'clear-display' to clear screen and scroll
    buffer. Add comment to use 'clear-screen' when 'clear-display' is not
    available.

--- bashrc/dot-config/bashrcparts/keymaps/keymaps.bash
+++ bashrc/dot-config/bashrcparts/keymaps/keymaps.bash
@@ -1,2 +1,5 @@
-# Add ctrl + l keymap to bash if not exists on remote servers
-bind -x '"\C-l": clear;'
+# Map CTRL-L to bash readline command 'clear-display'
+# NOTE: Use clear-screen when clear-display is not available e.g. in bash v4.2+
+# clear-display readline is available in bash v5.1+
+#bind -x '"\C-l": clear-screen;'
+bind '"\C-l": clear-display'
