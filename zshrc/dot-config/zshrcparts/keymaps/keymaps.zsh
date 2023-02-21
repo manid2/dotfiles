5a391d9b8c131c5ee939038da1ee59214c95dec9
Mani Kumar <manid2.work@gmail.com>
2023-02-21 19:26:41 +0530

    wip: [fzf] Add fzf spell suggestions to zsh, vim

--- zshrc/dot-config/zshrcparts/keymaps/keymaps.zsh
+++ zshrc/dot-config/zshrcparts/keymaps/keymaps.zsh
@@ -6,6 +6,35 @@ bindkey -e                                                # emacs key bindings
 # source fzf key bindings
 if [ -f ~/.vim/plugged/fzf/shell/key-bindings.zsh ]; then
 	source ~/.vim/plugged/fzf/shell/key-bindings.zsh
+
+	# fzf check spelling with dictionary preview.
+	__fspell () {
+		local cmd="command cat /usr/share/dict/words 2>/dev/null"
+		local item
+		eval "$cmd" | \
+		    FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} \
+		    --prompt 'dict> ' --reverse --border --ansi \
+		    --preview-label 'Dictionary' --preview-window '80%,wrap' \
+		    --preview 'dictls {}' \
+		    ${FZF_DEFAULT_OPTS-}" $(__fzfcmd) -m "$@" | \
+		    xargs -d "\n" -I {} echo -n "{} " | \
+		    xclip -sel clip -i
+		local ret=$?
+		return $ret
+	}
+
+	fzf-spell-widget () {
+		__fspell
+		local ret=$?
+		zle reset-prompt
+		return $ret
+	}
+
+	# alt+[ s
+	zle     -N              fzf-spell-widget
+	bindkey -M emacs '^[[s' fzf-spell-widget
+	bindkey -M vicmd '^[[s' fzf-spell-widget
+	bindkey -M viins '^[[s' fzf-spell-widget
 fi
 
 # custom command keybindings
