150232dfc0aa2c66aac852dd1af799fe1e4672ef
Mani Kumar <manid2.work@gmail.com>
2023-03-09 16:52:40 +0530

    fix: [shcmn] Add less terminal capabilities

--- dot-config/shellcommon/aliases/aliases.sh
+++ dot-config/shellcommon/aliases/aliases.sh
@@ -2,19 +2,28 @@
 
 # enable color support of ls, less and man, and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    if [ -r ~/.dircolors ]; then
-	    eval "$(dircolors -b ~/.dircolors)"
-    else
-	    eval "$(dircolors -b)"
-    fi
+	if [ -r ~/.dircolors ]; then
+		eval "$(dircolors -b ~/.dircolors)"
+	else
+		eval "$(dircolors -b)"
+	fi
 
-    alias ls='ls --color=always'
-    alias ip='ip --color=auto'
-    alias diff='diff --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias zgrep='zgrep --color=auto'
+	# `less` terminal capabilities
+	export LESS_TERMCAP_mb=$'\E[1;31m'  # begin blink
+	export LESS_TERMCAP_md=$'\E[1;36m'  # begin bold
+	export LESS_TERMCAP_me=$'\E[0m'     # reset bold/blink
+	export LESS_TERMCAP_so=$'\E[01;33m' # begin reverse video
+	export LESS_TERMCAP_se=$'\E[0m'     # reset reverse video
+	export LESS_TERMCAP_us=$'\E[1;32m'  # begin underline
+	export LESS_TERMCAP_ue=$'\E[0m'     # reset underline
+
+	alias ls='ls --color=always'
+	alias ip='ip --color=auto'
+	alias diff='diff --color=auto'
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
+	alias zgrep='zgrep --color=auto'
 fi
 
 # More `ls` aliases
