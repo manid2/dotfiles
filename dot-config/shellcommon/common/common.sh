41e9802701ebfaded2b144e987eae8691178af38
Mani Kumar <manid2.work@gmail.com>
2023-03-11 13:18:36 +0530

    feat: [install] Echo install messages with colors

--- dot-config/shellcommon/common/common.sh
+++ dot-config/shellcommon/common/common.sh
@@ -1,3 +1,14 @@
+# shellcheck shell=bash disable=SC2034
+# XDG base directories
+xdg_config="${XDG_CONFIG_HOME:-$HOME/.config}"
+xdg_data="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+msg_fmt () {
+	local file="$1"; shift
+	local msg="$*"
+	printf "${msg}\n" >> "$file"
+}
+
 # Workspace aliases for faster navigation
 mwp="$HOME/Documents/myworkspace"
 export mwp
