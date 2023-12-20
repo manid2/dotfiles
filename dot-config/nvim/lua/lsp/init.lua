f78df11bb19807780b6065d9b3a25c487403ed45
Mani Kumar <manid2.work@gmail.com>
2023-12-20 20:47:35 +0530

    fix: [nvim] LSP Keymap to format buffer

--- dot-config/nvim/lua/lsp/init.lua
+++ dot-config/nvim/lua/lsp/init.lua
@@ -101,7 +101,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<space>oc', vim.lsp.buf.outgoing_calls, bufopts)
 	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+	vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 end
 
 -- TODO: Add LSP for javascript, babeljs and typescript.
