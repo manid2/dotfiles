a5dcda93d827eb3974c0868d5668e1ec21608837
Mani Kumar <manid2.work@gmail.com>
2024-01-29 14:40:44 +0530

    feat: [shcmn] Add lnr cmds to install local pkg

--- dot-config/shellcommon/plugins/lnr/lnr.plugin.sh
+++ dot-config/shellcommon/plugins/lnr/lnr.plugin.sh
@@ -16,6 +16,29 @@ lnr_bin () {
 	lnr_rld "$1" "$2" "$HOME/.local/bin"
 }
 
+lnr_lib () {
+	lnr_rld "$1" "$2" "$HOME/.local/lib"
+}
+
+lnr_man () {
+	lnr_rld "$1" "$2" "$HOME/.local/share/man/man$3"
+}
+
+lnr_man1 () {
+	lnr_man "$1" "$2" "1"
+}
+
+lnr_share () {
+	lnr_rld "$1" "$2" "$HOME/.local/share"
+}
+
+lnr_dir () {
+	for file in "$2"/*;
+	do
+		lnr_rld "$1" "$file" "$3"
+	done
+}
+
 lnr_target () {
 	local target="$3"
 	if [ -d "$target" ]; then
@@ -27,18 +50,34 @@ lnr_target () {
 	fi
 }
 
-lnr_dir () {
-	for file in "$2"/*;
-	do
-		lnr_rld "$1" "$file" "$3"
-	done
+lnr_target_share () {
+	local t=$(echo "$2" | sed "s/.*\/share/share/g")
+	t="$HOME/.local/$t"
+	lnr_target "$1" "$2" "$t"
+}
+
+lnr_target_share_all () {
+	while IFS= read -r file;
+        do
+		lnr_target_share "$1" "$file"
+	done < <(find "$2" -type f -printf "%p\n" | sort)
 }
 
 alias lnrs='lnr -s '
 alias lnrsf='lnr -sf '
 alias lnrb='lnr_bin -s '
 alias lnrbf='lnr_bin -sf '
-alias lnrt='lnr_target -s '
-alias lnrtf='lnr_target -sf '
+alias lnrl='lnr_lib -s '
+alias lnrlf='lnr_lib -sf '
+alias lnrm='lnr_man1 -s '
+alias lnrmf='lnr_man1 -sf '
+alias lnrsh='lnr_share -s '
+alias lnrshf='lnr_share -sf '
 alias lnrd='lnr_dir -s '
 alias lnrdf='lnr_dir -sf '
+alias lnrt='lnr_target -s '
+alias lnrtf='lnr_target -sf '
+alias lnrtsh='lnr_target_share -s '
+alias lnrtshf='lnr_target_share -sf '
+alias lnrtsa='lnr_target_share_all -s '
+alias lnrtsaf='lnr_target_share_all -sf '
