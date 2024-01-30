0d782bc3d50cb8af02274f4fea8114fa2a50ea4f
Mani Kumar <manid2.work@gmail.com>
2024-01-30 22:52:56 +0530

    feat: [xfce4] Add xfce4 app install

---
+++ dot-local/app.d/xfce/config/terminal/accels.scm
@@ -0,0 +1,4 @@
+; xfce-terminal  GtkAccelMap rc-file         -*- scheme -*-
+; Disable zoom in/out for ctrl +/- as we rarely change the font size.
+(gtk_accel_path "<Actions>/terminal-window/zoom-in" "")
+(gtk_accel_path "<Actions>/terminal-window/zoom-out" "")
