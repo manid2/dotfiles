854abc41bedb7725d45dfa1aee9035ec1f0e127f
Mani Kumar <manid2.work@gmail.com>
2024-03-27 23:05:55 +0530

    wip: feat: [zsh] Add latest kali configurations

--- dot-config/zshrcparts/plugins/plugins.zsh
+++ dot-config/zshrcparts/plugins/plugins.zsh
@@ -1,6 +1,14 @@
 # shellcheck shell=bash disable=all
 # zsh plugins file, to be sourced into ~/.zshrc
 
+# enable auto-suggestions based on the history
+if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+	source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+	# change suggestion color
+	ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
+fi
+
+
 # Source command-not-found plugin if available
 if [ -f /etc/zsh_command_not_found ]; then
 	source /etc/zsh_command_not_found
