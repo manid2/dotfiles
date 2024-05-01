c778982ee0c7fe5e478cf369044eaf744fb5626f
Mani Kumar <manid2.work@gmail.com>
2024-05-01 12:26:58 -0400

    fix: [install] Remove logfile, source colors

--- dot-config/shellcommon/common/common.sh
+++ dot-config/shellcommon/common/common.sh
@@ -17,12 +17,6 @@ fi
 xdg_config="${XDG_CONFIG_HOME:-$HOME/.config}"
 xdg_data="${XDG_DATA_HOME:-$HOME/.local/share}"
 
-msg_fmt () {
-	local file="$1"; shift
-	local msg="$*"
-	printf "${msg}\n" >> "$file"
-}
-
 # Workspace aliases for faster navigation
 export mwp="$HOME/Documents/myworkspace"
 export msp="$HOME/Downloads/softwares"
