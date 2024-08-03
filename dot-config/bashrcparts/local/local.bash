581e06b8e39fedb8fcb51e17a59437d0d047cee4
Mani Kumar <manid2.work@gmail.com>
2024-08-03 22:44:16 +0530

    fix: [bashrc] Remove vim plugged FZF in PATH
    This is handled in setup script by adding it to ~/.local/bin.

--- dot-config/bashrcparts/local/local.bash
+++ dot-config/bashrcparts/local/local.bash
@@ -1,6 +1,3 @@
 # shellcheck disable=SC1090
 # add user local bin path to PATH
 export PATH="$HOME/.local/bin:$PATH"
-
-# add fzf in vim-plug path to PATH
-export PATH="$HOME/.vim/plugged/fzf/bin:$PATH"
